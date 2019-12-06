58b0af389c741c6d22b7ecde5d820670548ab60c
Ruslan Temirkhanov <rusl0424@gmail.com>
2019-12-06 18:25:16 -0500

    Implemented HATEOS for user requests. Added link to all users into response body.

--- src/main/java/com/goruslan/restfulwebservices/user/UserResource.java
+++ src/main/java/com/goruslan/restfulwebservices/user/UserResource.java
@@ -1,7 +1,10 @@
 package com.goruslan.restfulwebservices.user;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 import com.goruslan.restfulwebservices.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -16,18 +19,28 @@ public class UserResource {
     @Autowired
     private UserDaoService service;
 
+
     @GetMapping(path = "/users")
     public List<User> getAllUsers() {
         return service.findAll();
     }
 
+
     @GetMapping("/users/{id}")
-    public User getUser(@PathVariable int id){
+    public Resource<User> getUser(@PathVariable int id){
         User user = service.findOne(id);
         if(user == null)
             throw new UserNotFoundException("id - " + id);
-        return user;
+
+        // "all-users", SERVER_PATH + "/users"
+        // getAllUsers
+        Resource<User> resource = new Resource<User>(user);
+        ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).getAllUsers());
+        resource.add(linkTo.withRel("all-users"));
+        // HATEOAS
+        return resource;
     }
+
 
     @PostMapping("/users")
     public ResponseEntity<Object> newUser(@Valid @RequestBody User user){
@@ -41,6 +54,7 @@ public class UserResource {
         // Returns 201 status if the user is created
         return ResponseEntity.created(location).build();
     }
+
 
     @DeleteMapping("/users/{id}")
     public void deleteUser(@PathVariable int id){
