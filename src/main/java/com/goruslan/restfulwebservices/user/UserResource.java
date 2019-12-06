4e6450bff222de93f29ac16e10087ff4e2561c14
Ruslan Temirkhanov <rusl0424@gmail.com>
2019-12-06 00:53:36 -0500

    Added mapping for Deleting a user.

--- src/main/java/com/goruslan/restfulwebservices/user/UserResource.java
+++ src/main/java/com/goruslan/restfulwebservices/user/UserResource.java
@@ -28,23 +28,25 @@ public class UserResource {
         return user;
     }
 
-
     @PostMapping("/users")
     public ResponseEntity<Object> newUser(@RequestBody User user){
         User savedUser = service.save(user);
-
         // Will add uri to new created user into the header
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}")
                 .buildAndExpand(savedUser.getId())
                 .toUri();
-
         // Returns 201 status if the user is created
         return ResponseEntity.created(location).build();
     }
 
-
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        User user = service.deleteById(id);
+        if(user == null)
+            throw new UserNotFoundException("id - " + id);
+    }
 
 
 }
